db9d2d019161b334bfd43d13ccc4fcf6690c9638
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-04-16 12:35:58 -0600

    Create main.py
    class no1

---
+++ 01/main.py
@@ -0,0 +1,13 @@
+age = 23
+name = 'Jose'
+lastName = "Dimas"
+integertwo = 20
+pointFlotant = 2.25
+ istrue = True
+print('my name is {} {} and I have {} years old.' .format(name,lastName,age))
+
+#parsing convert to int
+print(int(pointFlotant))
+
+''' coment multiple line
+ continue... '''
