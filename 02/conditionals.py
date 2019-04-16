2f20d54396399b03aa53dbc5b69637e3145bf1ec
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-04-16 16:22:20 -0600

    Update conditionals.py
    conditional multiple

--- 02/conditionals.py
+++ 02/conditionals.py
@@ -1,8 +1,11 @@
 # conditionals
 dato1 = int (input('Ingresa un numero:'))
 dato2 = int (input('Ingresa otro numero:'))
+dato3 = int (input('Ingresa otro numero mas:'))
 tipo = input('Ingresa Cualquiera de los siguientes simbolos "+" "-" "*" "/": ')
 #conditional
+if(dato1==dato1==dato3):
+    print('Los numeros ingresados son iguales')
 
 if(tipo=='+'):
     res = dato1+dato2
