27867da869555d61fa937bc2092f0ebc2dd74c5d
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-04-23 10:25:50 -0600

    added new files
    learn cicles in python

---
+++ 02/cicles.py
@@ -0,0 +1,7 @@
+
+minombre = 'Jose Luis'
+for caracter in minombre:
+    print(caracter)
+
+for numero in range(0,10):
+    print(numero)
---
+++ 02/loops.py
@@ -0,0 +1,13 @@
+cont = 1
+while(cont<=5):
+    print('numero ',cont)
+    #  cont = cont +1
+    cont +=1
+
+# while reverse 
+
+cont2 = 5 
+while(cont2 >=0):
+    print('de reversa ',cont2)
+    cont2 =cont2 -1
+    # cont -= 1
