9eac5690ea924448db974005f8d0c8075a9fabed
Ariel Monterroso <bmonterrosoalvarado@gmail.com>
2019-04-29 17:07:26 -0600

    added methods for list
    adden new methods

--- 01/main.py
+++ 01/main.py
@@ -3,7 +3,7 @@ name = 'Jose'
 lastName = "Dimas"
 integertwo = 20
 pointFlotant = 2.25
- istrue = True
+istrue = True
 print('my name is {} {} and I have {} years old.' .format(name,lastName,age))
 
 #parsing convert to int
--- 03/lists.py
+++ 03/lists.py
@@ -6,16 +6,39 @@ lista = ['lista',1,25.6,True]
 
 # to change 
 lista[1] = 'nuevo dato'
+
 #print(lista[1])
 
 # add elements to list
-
 lista.append(25)
 
-for element in lista:
-    print(element)
+#show list
 
-# to delete 
+''''for element in lista:
+    print(element)'''
+
+# to delete to end
 print(lista)
 lista.pop()
 print(lista)
+
+# join list
+listaB = ['lista2',False,365] 
+lista_num = [25,36,15,1,3,7,23]
+lista.extend(listaB)
+print(lista)
+
+#delete elements in list
+del lista[5]
+#other mode to delete ////
+# lista.remove(5)
+print(lista)
+lista_num.sort()
+
+# lista_num.sort(reverse= True)
+print(lista_num)
+
+
+# index negative 
+
+print(lista_num[-2])
